e39d0b8f4c5be5b0e8e0b667996bac6be09c08d1
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-08-10 14:23:05 +0700

    FX

--- clean.bat
+++ clean.bat
@@ -3,5 +3,5 @@ git checkout --orphan latest_branch
 git add -A
 git commit -am "Re-index Repository"
 rem git branch -D master
-git branch -m master
+rem git branch -m master
 git push -f origin master
