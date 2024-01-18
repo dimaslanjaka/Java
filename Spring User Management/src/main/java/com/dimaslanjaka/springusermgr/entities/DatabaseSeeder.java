74069725033776a2c63160d0a68a1e0ae9b6182f
dimaslanjaka <dimaslanjaka@gmail.com>
2024-01-18 15:49:10 +0700

    add database seeder

---
+++ Spring User Management/src/main/java/com/dimaslanjaka/springusermgr/entities/DatabaseSeeder.java
@@ -0,0 +1,145 @@
+package com.dimaslanjaka.springusermgr.entities;
+
+// create default user and admin account
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DatabaseSeeder implements ApplicationListener<ContextRefreshedEvent> {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseSeeder(RoleRepository roleRepository, UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void onApplicationEvent(@NotNull ContextRefreshedEvent contextRefreshedEvent) {
+        if (createRoles()) {
+            if (createAdmin() && createUser() && createMultiAuthoritiesUser()) {
+                System.out.println("default accounts created successfully");
+            }
+        }
+    }
+
+    private boolean createMultiAuthoritiesUser() {
+        User user = new User();
+        user.setName("multiple authorities user");
+        user.setId(3L);
+        user.setAddress("unknown");
+        user.setEmail("multi@webmanajemen.com");
+        user.setPhone("00000000");
+        user.setAge(20);
+        user.setPassword(passwordEncoder.encode("multi"));
+        user.setGender("MALE");
+        Optional<Role> role = Optional.ofNullable(roleRepository.findByName("ROLE_USER"));
+        Optional<Role> role2 = Optional.ofNullable(roleRepository.findByName("ROLE_ADMIN"));
+        Optional<User> optionalUser = Optional.ofNullable(userRepository.findUserByEmail(user.getEmail()));
+        if (role.isEmpty() || role2.isEmpty()) {
+            System.out.println("role not yet created");
+            return false;
+        } else if (optionalUser.isPresent()) {
+            System.out.println("user with email " + user.getEmail() + " already exist");
+            return false;
+        } else {
+            List<Role> roles = new ArrayList<>();
+            roles.add(role.get());
+            roles.add(role2.get());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    /**
+     * create default user account
+     */
+    private boolean createUser() {
+        User user = new User();
+        user.setName("user");
+        user.setId(2L);
+        user.setAddress("unknown");
+        user.setEmail("user@webmanajemen.com");
+        user.setPhone("00000000");
+        user.setAge(20);
+        user.setPassword(passwordEncoder.encode("user"));
+        user.setGender("MALE");
+        Optional<Role> role = Optional.ofNullable(roleRepository.findByName("ROLE_USER"));
+        Optional<User> optionalUser = Optional.ofNullable(userRepository.findUserByEmail(user.getEmail()));
+        if (role.isEmpty()) {
+            System.out.println("role not yet created");
+            return false;
+        } else if (optionalUser.isPresent()) {
+            System.out.println("user with email " + user.getEmail() + " already exist");
+            return false;
+        } else {
+            List<Role> roles = new ArrayList<>();
+            roles.add(role.get());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    /**
+     * create admin account
+     */
+    private boolean createAdmin() {
+        User user = new User();
+        user.setName("admin");
+        user.setId(1L);
+        user.setAddress("unknown");
+        user.setEmail("admin@webmanajemen.com");
+        user.setPhone("00000000");
+        user.setAge(20);
+        user.setPassword(passwordEncoder.encode("admin"));
+        user.setGender("MALE");
+        Optional<Role> role = Optional.ofNullable(roleRepository.findByName("ROLE_ADMIN"));
+        Optional<User> optionalUser = Optional.ofNullable(userRepository.findUserByEmail(user.getEmail()));
+        if (role.isEmpty()) {
+            System.out.println("role not yet created");
+            return false;
+        } else if (optionalUser.isPresent()) {
+            System.out.println("user with email " + user.getEmail() + " already exist");
+            return false;
+        } else {
+            List<Role> roles = new ArrayList<>();
+            roles.add(role.get());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    /**
+     * create default roles (ROLE_ADMIN, ROLE_USER)
+     */
+    private boolean createRoles() {
+        Role role = new Role();
+
+        try {
+            role.setId(1L);
+            role.setName("ROLE_ADMIN");
+            roleRepository.save(role);
+
+            role.setId(2L);
+            role.setName("ROLE_USER");
+            roleRepository.save(role);
+            return true;
+        } catch (Exception ignored) {
+            return false;
+        }
+    }
+}
